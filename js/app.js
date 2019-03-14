435a2a2aff37d73ad5de4325bd5164fd632d9988
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-03-13 19:42:41 -1000

    Problem 2 Done

--- js/app.js
+++ js/app.js
@@ -8,8 +8,10 @@ var finePrint = 'Coupons VALID from Tuesday 12/4/2018 thru Saturday 6/29/2019. A
 document.getElementById("disclaimer").innerHTML = finePrint;
 //2. Target the div elemenet with the id of 'brand1' and update the content to Nabisco.
 
+document.getElementById("brand1").innerHTML = "Nabisco";
 //3. Target the div element with the id of 'item1' and update the content with Cheeseburger Oreos. 
 
+document
 //4. Target the div element with the id of 'price1' and update the content to $8.99.
 
 //5. Target the button element with the id of 'discount1' and update the content to Free fries with purchase!
