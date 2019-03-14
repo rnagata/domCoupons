1a74c5d5cb57db952eb9d004a9befca22fcef85f
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-03-13 19:39:17 -1000

    Problem 1 Done

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 //1. Target the h3 element with the id of 'disclaimer' and change the contents to the follow finePrint variable.
 
 var finePrint = 'Coupons VALID from Tuesday 12/4/2018 thru Saturday 6/29/2019. All sales FINAL. NO REFUNDS and EXCHANGES. And definitely NO RAINCHECKS! And ONLY POSITIVE reviews allowed on Social Media (FB, Instagram, Twitter, Yelp, etc.). And PLEASE Follow us on FB, Instagram, Twitter.'
-
+document.getElementById("disclaimer").innerHTML = finePrint;
 //2. Target the div elemenet with the id of 'brand1' and update the content to Nabisco.
 
 //3. Target the div element with the id of 'item1' and update the content with Cheeseburger Oreos. 
